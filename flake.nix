22d6d299d20214ff21ea320a376a8b0a604e757b
Copper Phosphate <cu3po42@gmail.com>
2023-06-30 21:44:08 +0200

    feat: add Nix flake for QMK

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Just a dev shell for QMK";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell { packages = [ pkgs.qmk ]; };
+      };
+    };
+}
